4ccf96025b4625c976f191073660b4b95477a9ac
maevet78 <190439mt@saintviator.com>
2018-08-14 21:10:51 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    "Hello, Wolrd!"
+  end
+  get '/name' do
+    "My name is Maeve"
+  end  
+  get '/hometown' do
+    "My hometown is Arlington Heights"
+  end
+  get '/favorite-song'
+    "My favorite song is Gateaway Car"
+  end  
 end
